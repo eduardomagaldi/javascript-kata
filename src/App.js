253bb9ba26a39f18c0fbab04ce21d68e19c42696
Eduardo Magaldi <magaldi1989@gmail.com>
2020-05-30 16:18:55 +0200

    filtering data via search

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import DataService from './services/Data.service';
 export default function App() {
   const [isLoaded, setIsLoaded] = React.useState(false);
   const [items, setItems] = React.useState([]);
+  const [allItems, setAllItems] = React.useState([]);
 
   React.useEffect(() => {
     Promise.all([
       DataService.fetchBooks(),
       DataService.fetchMagazines()
     ]).then(function (valArray) {
-      console.log('valArray', valArray);
       const data = valArray[0].concat(valArray[1]);
 
       data.sort((a, b) => {
@@ -25,28 +25,50 @@ export default function App() {
         return 0;
       });
 
+      setAllItems(data.slice());
 
       setIsLoaded(true);
       setItems(data);
     });
   }, []);
 
+  function handleKeyUp(e) {
+    const value = e.target.value;
+
+    if (value) {
+      const filtered = items.filter((item) => {
+        return item.title.indexOf(e.target.value) > -1;
+      });
+
+      setItems(filtered);
+    } else {
+      setItems(allItems);
+    }
+  }
+
   if (!isLoaded) {
     return <div>Loading...</div>;
   } else {
     return (
-      <ul>
-        {items.map(item => (
-          <li key={item.isbn}>
-            <strong>Title:</strong> {item.title}<br />
-            <strong>Authors:</strong> {item.authors}<br />
-            <strong>Description:</strong> {item["description\r"]}<br />
-            <strong>ISBN:</strong> {item.isbn}<br />
-            <br/>
-            <br/>
-          </li>
-        ))}
-      </ul>
+      <div>
+        <input
+          type="text"
+          onKeyUp={handleKeyUp}
+          />
+
+        <ul>
+          {items.map(item => (
+            <li key={item.isbn}>
+              <strong>Title:</strong> {item.title}<br />
+              <strong>Authors:</strong> {item.authors}<br />
+              <strong>Description:</strong> {item["description\r"]}<br />
+              <strong>ISBN:</strong> {item.isbn}<br />
+              <br/>
+              <br/>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
